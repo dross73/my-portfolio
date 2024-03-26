b0cc748367fa4aca87617ba2897394c92bd3d3d2
Dan Ross <evildead73@gmail.com>
2024-03-26 16:58:34 -0500

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {'path': '', component:AboutComponent},
+  {'path':'about', component:AboutComponent},
+  {'path':'contact', component:ContactComponent},
+  {'path':'projects', component:ProjectsComponent},
+  {'path':'resume', component:ResumeComponent},
+
+];
